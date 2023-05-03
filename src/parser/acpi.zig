c5c6b142595113dd112f6f710a544cf18d1d552a
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-05-03 10:28:40 +0800

    refactor(parser/acpi): uniform struct

--- src/parser/acpi.zig
+++ src/parser/acpi.zig
@@ -9,33 +9,8 @@ pub const TripPoints = struct {
 	temperature_unit: string
 };
 
-pub const Acpi = packed struct {
-	type: string,
-	id: i32,
-	state: string,
-	charge_precent: i32,
-	until_charged: string,
-	until_charged_hours: i32,
-	until_charged_minutes: i32,
-	until_charged_seconds: i32,
-	until_charged_total_seconds: i32,
-	charge_remaining: string,
-	charge_remaining_hours: i32,
-	charge_remaining_minutes: i32,
-	charge_remaining_seconds: i32,
-	charge_remaining_total_seconds: i32,
-	design_capacity_mah: i32,
-	last_full_capacity: i32,
-	last_full_capacity_precent: i32,
-	online: bool,
-	mode: string,
-	temperature: f64,
-	temperature_unit: string,
-	trip_points: []TripPoints,
-	messages: []string
-};
 
-pub const AcpiJson = packed struct {
+pub const AcpiJson = struct {
 	type: string,
 	id: i32,
 	state: string,
