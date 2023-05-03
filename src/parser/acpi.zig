0378ab453574ceee0115764373d5366c44f166f5
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-05-03 09:50:08 +0800

    fix(parser/acpi): zig json property name workaround

--- src/parser/acpi.zig
+++ src/parser/acpi.zig
@@ -2,14 +2,14 @@ const std = @import("std");
 
 const string = []u8;
 
-const TripPoints = struct {
+pub const TripPoints = struct {
 	id: i32,
 	switches_to_mode: string,
 	temperature: f64,
 	temperature_unit: string
 };
 
-const Acpi = struct {
+pub const Acpi = packed struct {
 	type: string,
 	id: i32,
 	state: string,
@@ -28,6 +28,32 @@ const Acpi = struct {
 	last_full_capacity: i32,
 	last_full_capacity_precent: i32,
 	online: bool,
+	mode: string,
+	temperature: f64,
+	temperature_unit: string,
+	trip_points: []TripPoints,
+	messages: []string
+};
+
+pub const AcpiJson = packed struct {
+	type: string,
+	id: i32,
+	state: string,
+	charge_precent: i32,
+	until_charged: string,
+	until_charged_hours: i32,
+	until_charged_minutes: i32,
+	until_charged_seconds: i32,
+	until_charged_total_seconds: i32,
+	charge_remaining: string,
+	charge_remaining_hours: i32,
+	charge_remaining_minutes: i32,
+	charge_remaining_seconds: i32,
+	charge_remaining_total_seconds: i32,
+	design_capacity_mah: i32,
+	last_full_capacity: i32,
+	last_full_capacity_precent: i32,
+	@"on-line": bool,
 	mode: string,
 	temperature: f64,
 	temperature_unit: string,
